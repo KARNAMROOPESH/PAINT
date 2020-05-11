d67b9ef663b6e5c24e789538e2183adb4d084adf
KARNAMROOPESH <karnamroop1234@gmail.com>
2020-05-11 17:28:38 +0530

    made the balls on both the screens to sync

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 var ball;
+var database;
+var position;
 
 function setup(){
+    database = firebase.database();
+    console.log(database);
     createCanvas(500,500);
+    
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballref = database.ref('ball/position');
+    ballref.on("value",readPosition,showError);
 }
 
 function draw(){
@@ -24,6 +31,18 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({  
+        'x':position.x + x,
+        'y':position.y + y
+    });
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("error connecting to the database");
 }
