928a24d1b6c1fc8a60eca22ae343ae976c61b437
KARNAMROOPESH <karnamroop1234@gmail.com>
2020-05-18 20:35:22 +0530

    made painting

--- sketch.js
+++ sketch.js
@@ -1,58 +1,30 @@
-var ball;
+
 var database;
-var position;
-var smoke;
-var tra = [];
-var position = [];
+var drawing = [] ;
 
 function setup(){
     database = firebase.database();
     console.log(database);
-    createCanvas(400,400);
-    
-    ball = createSprite(200,200,10,10);
-    ball.shapeColor = "black";
-    var ballref = database.ref('ball/position');
-    ballref.on("value",readPosition,showError);
-
-    smoke = loadImage("smoke.png");
-
-    position.push(ball.position.x, ball.position.y,ball.x,ball.y)
-    tra.push(position);
-    console.log(tra);
-    console.log(position);
+    createCanvas(1000,500);
+    console.log(drawing);
 }
 
 function draw(){
-    background(255);
-   ball.x = mouseX;
-   ball.y = mouseY;
+   background(0);
 
-   for (var i = 1; i<tra.length ; i++) {
-    
-  image(smoke, tra[i][0], tra[i][1]);
-  }
-    drawSprites();
+ if(mouseIsPressed){
+   var point = {
+     x: mouseX,
+     y: mouseY
+   }
+   drawing.push(point);
 }
-
-function changePosition(x,y){
-    database.ref('ball/position').set({  
-        'x':position.x + x,
-        'y':position.y + y
-    });
-}
-
-function readPosition(data){
-    position = data.val();
-    ball.x = position.x;
-    ball.y = position.y;
-}
-
-function showError(){
-    console.log("error connecting to the database");
-}
-
-function mouseDragged(){
-   
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+beginShape();
+stroke(255);
+strokeWeight(4);
+noFill();
+ for (var i = 0 ; i < drawing.length ;i++){
+  vertex(drawing[i].x , drawing[i].y)
+ }
+endShape();
 }
